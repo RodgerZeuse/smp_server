9bdb0af8253dbaa9d1771c42261f2295b2a2b05e
Rodger Zeuce <rodgerzeuse@gmail.com>
2019-04-10 10:41:09 +0500

    18th commit

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
-'use strict';
+"use strict";
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
-var express = require('express');
-var request = require('request');
-var app = module.exports = loopback();
-app.set('view engine', 'ejs');
+var loopback = require("loopback");
+var boot = require("loopback-boot");
+var express = require("express");
+var request = require("request");
+var app = (module.exports = loopback());
+process.env.PWD = process.cwd();
+app.set("view engine", "ejs");
 // app.use(express.static(__dirname+'/views/app-preview'));
 // console.log("directory path",__dirname+'/views/app-preview')
 // app.use(express.static(__dirname+'/views/app-preview'));
 // app.use(express.static('/Users/yamin/Documents/workspace/SocialMediaPoster../server/server/views'));
-require('loopback-ds-tree-mixin')(app);
+require("loopback-ds-tree-mixin")(app);
 // app.start = function() {
 //   // start the web server
 //   return app.listen(function() {
@@ -23,7 +24,6 @@ require('loopback-ds-tree-mixin')(app);
 //     }
 //   });
 // };
-
 
 // app.start = () => {
 //   app.server = app.listen(() => {
@@ -41,20 +41,27 @@ require('loopback-ds-tree-mixin')(app);
 //   return app.server
 // }
 
-
 app.start = function() {
   // start the web server
   var server = app.listen(function() {
-    app.emit('started', server);
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    app.emit("started", server);
+    var baseUrl = app.get("url").replace(/\/$/, "");
+    console.log("Web server listening at: %s", baseUrl);
+    if (app.get("loopback-component-explorer")) {
+      var explorerPath = app.get("loopback-component-explorer").mountPath;
+      console.log("Browse your REST API at %s%s", baseUrl, explorerPath);
     }
   });
   return server;
 };
+
+var ds = loopback.createDataSource({
+  connector: require("loopback-component-storage"),
+  provider: "filesystem",
+  root: path.join(process.env.PWD + "/local-storage")
+});
+
+var container = ds.createModel("Container");
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
@@ -62,10 +69,9 @@ boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module){
-
+  if (require.main === module) {
     // app.start();
-    app.io = require('socket.io')(app.start());
+    app.io = require("socket.io")(app.start());
     // console.log('app start =>',app.io);
     // app.io = require('socket.io')(app.start());
 
@@ -85,34 +91,30 @@ boot(app, __dirname, function(err) {
     //         } else {
     //           callback(null, false);
     //         }
-    //       });   
+    //       });
     //     } //authenticate function..
     // });
-      // app.io.on('connection', function(socket){
-      //   console.log('a user connected',socket.id);
-      //   // console.log(app.models.Todo)
-      //   socket.on('message',function(data){
-      //     console.log("socket io connected");
-      //     app.models.Campaign.findOne( {where: {id: data},include:{"campaignComments":"registerUser"}},(err,commnets) =>{
-            
+    // app.io.on('connection', function(socket){
+    //   console.log('a user connected',socket.id);
+    //   // console.log(app.models.Todo)
+    //   socket.on('message',function(data){
+    //     console.log("socket io connected");
+    //     app.models.Campaign.findOne( {where: {id: data},include:{"campaignComments":"registerUser"}},(err,commnets) =>{
 
-      //       app.io.emit('message', commnets);
-      //     //   if(err){
-      //     //   console.log("getting error from fetching campaign")
-      //     //  }else{
-      //     //   console.log("real time get campaign",commnets);
-      //     //   app.io.emit('message', commnets);
-      //     //  }
-            
-      //     })
-          
-          
-         
-      //   }).setMaxListeners(100000);
-      //   socket.on('disconnect', function(){
-      //       console.log('user disconnected');
-      //   });
-      // });
-   
+    //       app.io.emit('message', commnets);
+    //     //   if(err){
+    //     //   console.log("getting error from fetching campaign")
+    //     //  }else{
+    //     //   console.log("real time get campaign",commnets);
+    //     //   app.io.emit('message', commnets);
+    //     //  }
+
+    //     })
+
+    //   }).setMaxListeners(100000);
+    //   socket.on('disconnect', function(){
+    //       console.log('user disconnected');
+    //   });
+    // });
   }
 });
